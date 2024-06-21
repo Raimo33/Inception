ab4a246caa5c976988c8ab73cc9ba35ead0b7642
Claudio <claudio.raimondi@protonmail.com>
2024-06-21 18:08:26 +0200

    Minor changes

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -3,13 +3,13 @@
 /usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
 pid="$!"
 
-sleep 10
+sleep 20
 
 envsubst < create_db.sql > create_db.sql
 envsubst < create_user.sql > create_user.sql
-mysql -u root < create_db.sql
-mysql -u root < create_user.sql
+mysql -u root -p${DB_ROOT_PASSWORD} < create_db.sql
+mysql -u root -p${DB_ROOT_PASSWORD} < create_user.sql
 
-mysqladmin -u root shutdown
+mysqladmin -u root -p${DB_ROOT_PASSWORD} -h localhost shutdown
 
 wait "$pid"
