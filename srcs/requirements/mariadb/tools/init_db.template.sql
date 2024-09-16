7f034e4e89456aa891c658986afff3601cd1c911
Claudio <claudio.raimondi@protonmail.com>
2024-09-16 14:51:05 +0200

    Remove reduntant options in vsftpd.conf, Add pam file, Separate user creation in vsftpd

--- srcs/requirements/mariadb/tools/init_db.template.sql
+++ srcs/requirements/mariadb/tools/init_db.template.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `${DB_NAME}`;
 
-CREATE USER '${DB_USER}'@'wordpress.backend' IDENTIFIED BY '${DB_PASSWORD}';
-CREATE USER '${DB_USER}'@'adminer.backend' IDENTIFIED BY '${DB_PASSWORD}';
+CREATE USER '${DB_USER}'@'wordpress.backend' IDENTIFIED BY '${DB_USER_PASSWORD}';
+CREATE USER '${DB_USER}'@'adminer.backend' IDENTIFIED BY '${DB_USER_PASSWORD}';
 CREATE USER 'ping-user'@'wordpress.backend' IDENTIFIED BY '';
 
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON `${DB_NAME}`.* TO '${DB_USER}'@'wordpress.backend';
---
+++ srcs/requirements/vsftpd/tools/create_users.sh
@@ -0,0 +1,26 @@
+#!/bin/ash
+
+create_ftp_user()
+{
+	user=$1
+	password=$2
+
+	if id "$user" >/dev/null 2>&1; then
+		echo "User $user already exists"
+        return
+    fi
+
+	adduser -D $user
+	echo "$user:$password" | chpasswd
+	
+	home_dir="/home/$user"
+	mkdir -p $home_dir/files
+	chown -R $user:$user $home_dir/files
+
+	chmod 555 $home_dir
+	chmod 775 $home_dir/files
+
+	echo "User $user created with writable subdirectory $home_dir/files"
+}
+
+create_ftp_user "$FTP_USER" "$FTP_USER_PASSWORD"
